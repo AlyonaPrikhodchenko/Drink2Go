b4333dab1a4232dcffe7dd2222d9970e01b12203
Alyona <alenka.inform@gmail.com>
2022-10-10 17:52:01 +0300

    Updates

---
+++ js/swiper.js
@@ -0,0 +1,1 @@
+const openSwiper=()=>{new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},initialSlide:0})};export{openSwiper};
